a0192f1df16aa23fd77559f2a1402f7e7b6d95cc
The Administrator <125167611+rootgov@users.noreply.github.com>
2023-05-02 06:18:31 -0700

    Update setup.py
    Signed-off-by: The Administrator <125167611+rootgov@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-import os
-
+iimport os
 import setuptools.command.build_py
 import setuptools.command.develop
 from setuptools import find_packages, setup
@@ -10,8 +9,7 @@ cwd = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(cwd, "VERSION")) as fin:
     version = fin.read().strip()
 
-
-class build_py(setuptools.command.build_py.build_py):  # pylint: disable=too-many-ancestors
+class build_py(setuptools.command.build_py.build_py):
     def run(self):
         self.create_version_file()
         setuptools.command.build_py.build_py.run(self)
@@ -23,49 +21,31 @@ class build_py(setuptools.command.build_py.build_py):  # pylint: disable=too-man
         with open(version_path, "w") as f:
             f.write("__version__ = '{}'\n".format(version))
 
-
 class develop(setuptools.command.develop.develop):
     def run(self):
         build_py.create_version_file()
         setuptools.command.develop.develop.run(self)
 
-
 requirements = open(os.path.join(cwd, "requirements.txt"), "r").readlines()
+
 with open("README.md", "r", encoding="utf-8") as readme_file:
     README = readme_file.read()
-
 
 setup(
     name="coqpit",
     version=version,
-    url="https://github.com/erogol/coqpit",
+    url="https://github.com/knowledge-gov/coqpit",
     author="Eren Gölge",
     author_email="egolge@coqui.ai",
-    description="Simple (maybe too simple), light-weight config management through python data-classes.",
+    description="A lightweight Python library for managing configuration files using data classes.",
     long_description=README,
     long_description_content_type="text/markdown",
-    license="",
+    author2="The Administrator",
+    author_email2="admin@lifeip.cloud",
+    project_urls={
+        "Issue Tracker": "https://github.com/knowledge-gov/coqpit/issues"
+    },
     include_package_data=True,
     packages=find_packages(include=["coqpit*"]),
-    project_urls={
-        "Tracker": "https://github.com/coqui-ai/coqpit/issues",
-        "Repository": "https://github.com/coqui-ai/coqpit",
-        "Discussions": "https://github.com/coqui-ai/coqpit/discussions",
-    },
-    cmdclass={
-        "build_py": build_py,
-        "develop": develop,
-    },
-    install_requires=requirements,
-    python_requires=">=3.7.0",
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS",
-        "Operating System :: Microsoft :: Windows",
-    ],
-    zip_safe=False,
+    install_requires=requirements
 )
